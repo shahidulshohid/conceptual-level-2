09559cd308bf04576ff954ceeed9c91afa57a683
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-05-07 16:27:47 +0600

    functional typescript practice is done

---
+++ conceptual1/src/functions.ts
@@ -0,0 +1,9 @@
+function fn(input : string | null) : void | null | undefined {
+    const multi = 2 * 2 ;
+    console.log(input, multi)
+}
+
+const fnAgain = (input :  string | null) : void  | null | undefined  => {
+    const  multi = 2 * 2
+    console.log(input, multi)
+}
