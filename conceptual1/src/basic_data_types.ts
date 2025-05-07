e8a8b65a5e4e1b2091bb0217fb8a3ecf1adfa6cb
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-05-07 10:19:25 +0600

    start conceptual practice

---
+++ conceptual1/src/basic_data_types.ts
@@ -0,0 +1,26 @@
+const srt : string = "Abrar"
+// primitive data types
+// ss bb u nn = string, symbol, bigint, boolean, number, undefine
+// non-primitive data types
+// array, object
+
+let anyVar : number = 1;
+
+let arr : number[] = [1,2,3]
+
+export type TObj = {
+    name: string;
+    age: number;
+    education?: {
+        institute: string;
+        level: string;
+    }
+}
+export const obj : TObj = {
+    name: "Munsur",
+    age: 30,
+    education: {
+        institute: "University",
+        level: "Undergrad",
+    }
+}
---
+++ conceptual1/src/test.ts
@@ -0,0 +1,10 @@
+import { TObj } from "./basic_data_types"
+
+const test : string = "test"
+console.log(test)
+
+const obj2 : TObj = {
+    name: "Hasibul bhi",
+    age: 33,
+}
+console.log(obj2)
